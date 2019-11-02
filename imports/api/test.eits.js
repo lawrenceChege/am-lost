c3ff45ac01bc25630e26e3679227fc6839c9711b
lawrence-chege <lawrence.chege@meltwater.org>
2019-11-02 13:09:26 +0000

    use double quotesand semicolons

--- imports/api/test.eits.js
+++ imports/api/test.eits.js
@@ -14,9 +14,9 @@ if (Meteor.isServer) {
         let user = Meteor.users.findOne({ username: username });
         if (!user) {
           userId = Accounts.createUser({
-            'username': username,
-            'email': 'larry@gmail.com',
-            'password': '12345578',
+            "username": username,
+            "email": "larry@gmail.com",
+            "password": "12345578",
 
           });
         } else {
@@ -28,62 +28,62 @@ if (Meteor.isServer) {
         beforeEach(() => {
             Eits.remove({});
             EitId = Eits.insert({
-              name: 'Dayo',
+              name: "Dayo",
               age: 31,
               phone: 16626278,
-              country: 'Nigeria',
-              area: 'Technology',
-              fact: 'Party hype',
+              country: "Nigeria",
+              area: "Technology",
+              fact: "Party hype",
               createdAt: new Date(),
               owner: userId,
               username: "larry"
             }
 
-            )
-            // Meteor.call('eits.insert' )
+            );
+            // Meteor.call("eits.insert" )
 
         });
-      it('can create an EIT', () => {
-        const insertEIT = Meteor.server.method_handlers['eits.insert'];
+      it("can create an EIT", () => {
+        const insertEIT = Meteor.server.method_handlers["eits.insert"];
         const invocation = { userId }
         insertEIT.apply(invocation, [{
-          name: 'Dayo',
+          name: "Dayo",
           age: 31,
           phone: 16626278,
-          country: 'Nigeria',
-          area: 'Technology',
-          fact: 'Party hype',
+          country: "Nigeria",
+          area: "Technology",
+          fact: "Party hype",
           createdAt: new Date(),
           owner: userId,
           username: "lawrence"
         }]);
-        assert.equal(Eits.find().count(),2)
+        assert.equal(Eits.find().count(),2);
       })
-      it('cannot create an EIT when not logged in', () => {
-        const insertEIT = Meteor.server.method_handlers['eits.insert'];
+      it("cannot create an EIT when not logged in", () => {
+        const insertEIT = Meteor.server.method_handlers["eits.insert"];
         const invocation = { }
 
         assert.throws(() => {
           insertEIT.apply(invocation, [{
-            name: 'Dayo',
+            name: "Dayo",
             age: 31,
             phone: 16626278,
-            country: 'Nigeria',
-            area: 'Technology',
-            fact: 'Party hype',
+            country: "Nigeria",
+            area: "Technology",
+            fact: "Party hype",
             createdAt: new Date(),
             owner: userId,
             username: "lawrence"
           }]);
-        }, Meteor.Error, 'You are not allowed to insert EIT')
+        }, Meteor.Error, "You are not allowed to insert EIT");
         
-        assert.equal(Eits.find().count(),1)
+        assert.equal(Eits.find().count(),1);
       })
         
-      it('can delete owned EIT', () => {
+      it("can delete owned EIT", () => {
                   // Find the internal implementation of the task method so we can
         // test it in isolation
-        const deleteTask = Meteor.server.method_handlers['eits.remove'];
+        const deleteTask = Meteor.server.method_handlers["eits.remove"];
  
         // Set up a fake method invocation that looks like what the method expects
         const invocation = { userId };
@@ -94,9 +94,9 @@ if (Meteor.isServer) {
         // Verify that the method does what we expected
         assert.equal(Eits.find().count(), 0);
       });
-      it('cannot delete someones EIT',() => {
+      it("cannot delete someones EIT",() => {
         // test it in isolation
-        const deleteTask = Meteor.server.method_handlers['eits.remove'];
+        const deleteTask = Meteor.server.method_handlers["eits.remove"];
  
         // Set up a fake method invocation that looks like what the method expects
         const invocation = { };
@@ -105,82 +105,82 @@ if (Meteor.isServer) {
         assert.throws(() => {
           deleteTask.apply(invocation, [EitId]);
 
-        }, Meteor.Error, 'You are not allowed to delete this')
+        }, Meteor.Error, "You are not allowed to delete this");
         
  
         // Verify that the method does what we expected
         assert.equal(Eits.find().count(), 1);
 
       });
-      it('can view all EITs',() => {
-        const userId = Random.id()
+      it("can view all EITs",() => {
+        const userId = Random.id();
         Eits.insert({
-          name: 'Dayo',
-          age: 31,
-          phone: 16626278,
-          country: 'Nigeria',
-          area: 'Technology',
-          fact: 'Party hype',
+          name: "Dayo",
+          age: "31",
+          phone: "16626278",
+          country: "Nigeria",
+          area: "Technology",
+          fact: "Party hype",
           createdAt: new Date(),
           owner: userId,
           username: "larry"
-        })
+        });
         const invocation = {userId}
-        const EITPublication = Meteor.server.publish_handlers.eits
-        assert.strictEqual(EITPublication.apply(invocation).count(),2)
+        const EITPublication = Meteor.server.publish_handlers.eits;
+        assert.strictEqual(EITPublication.apply(invocation).count(),2);
 
 
       });
-      it('can edit owned EIT',() => {
-        const editEITs = Meteor.server.method_handlers['eits.edit']
+      it("can edit owned EIT",() => {
+        const editEITs = Meteor.server.method_handlers["eits.edit"]
         const invocation = { userId }
         editEITs.apply(invocation, [EitId,{
-          name: 'Helen',
-          age: 23,
-          phone: 166262578,
-          country: 'Nigeria',
-          area: 'Technology',
-          fact: 'Party hype',
+          name: "Helen",
+          age: "23",
+          phone: "166262578",
+          country: "Nigeria",
+          area: "Technology",
+          fact: "Party hype",
           createdAt: new Date(),
           owner: userId,
           username: "larry"
         }])
-        assert.equal(Eits.find({name: 'Helen'}).count(), 1);
+        assert.equal(Eits.find({name: "Helen"}).count(), 1);
       });
-      it('cannot edit someones EIT',() => {
-        const editEITs = Meteor.server.method_handlers['eits.edit']
+      it("cannot edit someones EIT",() => {
+        const editEITs = Meteor.server.method_handlers["eits.edit"];
         const invocation = { }
 
         assert.throws(() => {
           editEITs.apply(invocation, [EitId,{
-            name: 'Helen',
-            age: 23,
-            phone: 166262578,
-            country: 'Nigeria',
-            area: 'Technology',
-            fact: 'Party hype',
+            name: "Helen",
+            age: "23",
+            phone: "166262578",
+            country: "Nigeria",
+            area: "Technology",
+            fact: "Party hype",
             createdAt: new Date(),
             owner: userId,
             username: "larry"
-          }])
-        }, Meteor.Error, 'You cannot edit someone else EIT')
+          }]);
+        }, Meteor.Error, "You cannot edit someone else EIT");
         
-        assert.equal(Eits.find({name: 'Helen'}).count(), 0);
+        assert.equal(Eits.find({name: "Helen"}).count(), 0);
 
       });
-      it('can check owned EIT',() => {
-        const setChecked = Meteor.server.method_handlers['eits.setChecked'];
-        const invocation = { userId }               
+      it("can check owned EIT",() => {
+        const setChecked = Meteor.server.method_handlers["eits.setChecked"];
+        const invocation = { userId };          
         setChecked.apply(invocation, [EitId, true]);
         assert.equal(Eits.find({checked:true}).count(), 1);                
 
       });
-      it('cannot check someones EIT',() => {
-        const setChecked = Meteor.server.method_handlers['eits.setChecked'];
-        const invocation = { }
-        assert.throws(()=>{
+      it("cannot check someones EIT",() => {
+        const setChecked = Meteor.server.method_handlers["eits.setChecked"];
+        const invocation = { };
+        assert.throws(() => {
           setChecked.apply(invocation, [EitId, true]);
-        }, Meteor.Error, 'You are not allowed to do that')         
+        }, Meteor.Error, "You are not allowed to do that");     
         
         assert.equal(Eits.find({checked:true}).count(), 0); 
 
--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import "../imports/api/test.eits"
+import "../imports/api/test.eits";
 
 describe("am-lost", function () {
   it("package.json has correct name", async function () {
